96de63b361ec336ceed45b0d5aba6a7c335bcf7f
Martin Nie <jnie@snapchat.com>
2017-12-21 10:50:03 +0800

    update

--- imagepicker/src/main/java/com/imnjh/imagepicker/PhotoLoadListener.java
+++ imagepicker/src/main/java/com/imnjh/imagepicker/PhotoLoadListener.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 
 import android.net.Uri;
 
-/**
- * Created by Martin on 2017/2/11.
- * niejunhong@panda.tv
- */
-
 public interface PhotoLoadListener {
   void onLoadComplete(ArrayList<Uri> photoUris);
 
